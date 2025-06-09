c69541e2f3cea2c6cdbf1ffff43c2041ae2a361b
richbl <richbl@gmail.com>
2025-06-08 20:49:17 -0700

    refactor(app): :art: Update to README and references to MIT license (was GNU v3); minor code cleanup; update to latest bash-lib
    Signed-off-by: richbl <richbl@gmail.com>

--- bash_lib_test_runner.sh
+++ bash_lib_test_runner.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # -----------------------------------------------------------------------------
-# Copyright (C) Business Learning Incorporated (businesslearninginc.com)
+# Copyright (c) 2025 Richard Bloch
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License at
-# <http://www.gnu.org/licenses/> for more details.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
 # -----------------------------------------------------------------------------
 #
 # Bash Test Runner for BASH-LIB libraries
 # Tests the functionality of the 'args' and 'general' bash-lib libraries
-#
+# Version 1.2.0
 
 # --- Configuration -----------------------------------------------------------
 #
